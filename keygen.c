bc29c4dfdf9425503dc5909d15b1b9c16f8752bb
jpulattie <97932025+jpulattie@users.noreply.github.com>
2025-03-12 22:56:06 +0000

    keygen fix

--- keygen.c
+++ keygen.c
@@ -10,13 +10,15 @@ char *generator(int num, char *key)
     int random_number;
     int max = 27;
     int min = 1;
+    printf("num: %d\n", num);
     // printf("num equals: %d\n", num);
     key[0] = '\0';
     // char convert[numb + 1];
     for (i = 0; i < num; i++)
     {
         random_number = (rand() % (max - min + 1) + min);
-        // printf("number: %d ", random_number);
+        printf("random number: %d \n", random_number);
+        printf("random + 64 %d\n", random_number + 64);
         if (random_number == 27)
         {
             key[i] = ' ';
@@ -30,7 +32,7 @@ char *generator(int num, char *key)
         {
             key[i] = random_number + 64;
         }
-        //key[num] = '\n';
+        key[num] = '\0';
         //printf("i %d: %c\n", i, key[i]);
     }
         //printf("-%d-", key[num]);
@@ -42,9 +44,9 @@ char *generator(int num, char *key)
 int main(int argc, char *argv[])
 {
     int randomGen = atoi(argv[1]);
-
+    printf("random gen in main: %d", randomGen);
     char key[randomGen];
-    //printf("random gen:%d\n", randomGen);
+    printf("length of key:%ld\n", strlen(key));
     int final;
     srand(time(NULL));
     generator(randomGen, key);
