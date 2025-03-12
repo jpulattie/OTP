ac6010fe4bab9596bee863880057fa2d9d5f1914
jpulattie <97932025+jpulattie@users.noreply.github.com>
2025-03-12 02:56:27 +0000

    key fix

--- keygen.c
+++ keygen.c
@@ -2,40 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-int generator(int num){
+char *generator(int num, char *key)
+{
     int i;
     char alpha;
     int random_number;
     int max = 27;
     int min = 1;
-    //printf("num equals: %d\n", num);
-    char key[num +1];
+    // printf("num equals: %d\n", num);
     key[0] = '\0';
-    //char convert[numb + 1];
-    for (i=0; i < num; i++) {
+    // char convert[numb + 1];
+    for (i = 0; i < num; i++)
+    {
         random_number = (rand() % (max - min + 1) + min);
-        //printf("number: %d ", random_number);
-        if (random_number == 27){
+        // printf("number: %d ", random_number);
+        if (random_number == 27)
+        {
             key[i] = ' ';
-        } 
-        //if (i == num -1){
-         //   printf("last cha\n");
-         //   key[i] = '\n';
-         //   printf("should be a blank line above\n");
+        }
+        // if (i == num -1){
+        //    printf("last cha\n");
+        //    key[i] = '\n';
+        //    printf("should be a blank line above\n");
         //}
-        else {
+        else
+        {
             key[i] = random_number + 64;
         }
         key[num] = '\0';
-        //printf("i %d: %c\n", i, key[i]);}
-        //printf("key: %s\n", key);
-    return 0;
-}
+        // printf("i %d: %c\n", i, key[i]);}
+        // printf("key: %s\n", key);
+        return key;
+    }
 }
 
-int main(int argc, char *argv[]) {
-    int final;
+int main(int argc, char *argv[])
+{
     int randomGen = atoi(argv[1]);
-    generator(randomGen);
+
+    char key[randomGen + 1];
+    int final;
+    generator(randomGen, key);
     return 0;
 }
